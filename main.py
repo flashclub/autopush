0b43230402b2877300cf41d99d7e19122b22fe64
梁长路 <liangcl@pingpongx.com>
2022-06-09 10:45:16 +0800

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import datetime,time
 import git
 def loop_task():
   while True:
-    print('执行循环-')
+    print('执行循环-2')
     repo = git.cmd.Git('~/Documents/code/timertask')
     repo.add('.')
     print('执行 add')

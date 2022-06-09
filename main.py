4bce7294aeac6fd0b05433a689d6227673c160cf
梁长路 <liangcl@pingpongx.com>
2022-06-09 10:49:14 +0800

    update

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-import datetime,time
+import datetime
+import time
 import git
-def loop_task():
-  while True:
-    print('执行循环-2')
-    repo = git.cmd.Git('~/Documents/code/timertask')
-    repo.add('.')
-    print('执行 add')
-    repo.commit('-m update')
-    print('执行 commit')
-    repo.push()
-    print('执行 push')
-    time.sleep(2)
+
+
+class AutoSave:
+    def __init__(self):
+        self.loop_task()
+
+    def loop_task(self):
+        while True:
+            print('执行循环-2')
+            try:
+                self.save_code()
+            except BaseException as e:
+                print(e)
+                pass
+            time.sleep(10)
+
+    def save_code(self):
+        repo = git.cmd.Git('~/Documents/code/timertask')
+        repo.add('.')
+        print('执行 add')
+        repo.commit('-m update')
+        print('执行 commit')
+        repo.push()
+        print('执行 push')
+
+
 if __name__ == '__main__':
-  loop_task()
+    AutoSave()
